5cd96536e65ba85a60d3053b7f928f5343d1467c
Sahil Verma <domainsahil007@gmail.com>
2026-09-16 14:38:59 +0530

    feat: implement longest consecutive sequence algorithm and enable test cases

--- 2026/sep-16/128. Longest Consecutive Sequence.py
+++ 2026/sep-16/128. Longest Consecutive Sequence.py
@@ -1,16 +1,28 @@
 class Solution:
     def longestConsecutive(self, nums: list[int]) -> int:
-        sorted_array = sorted(nums)
+        if not nums:
+            return 0
 
-        print(sorted_array)
+        nums.sort()
+        nums_set = set(nums)
 
-        return 0
+        max_length = 1
+        current_length = 1
+
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1] + 1:
+                current_length += 1
+            elif nums[i] != nums[i-1]:
+                max_length = max(max_length, current_length)
+                current_length = 1
+
+        return max(max_length, current_length)
 
 
 s = Solution()
 
 
 print("Example 1:", s.longestConsecutive(nums=[100, 4, 200, 1, 3, 2]))  # 4
-# print("Example 2:", s.longestConsecutive(
-#     nums=[0, 3, 7, 2, 5, 8, 4, 6, 0, 1]))  # 9
-# print("Example 3:", s.longestConsecutive(nums=[1, 0, 1, 2]))  # 3
+print("Example 2:", s.longestConsecutive(
+    nums=[0, 3, 7, 2, 5, 8, 4, 6, 0, 1]))  # 9
+print("Example 3:", s.longestConsecutive(nums=[1, 0, 1, 2]))  # 3
