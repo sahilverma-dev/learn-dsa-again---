7cbfc5dcf5abf1e1d3069c870aeaff59646834fa
Sahil Verma <domainsahil007@gmail.com>
2026-09-16 12:20:29 +0530

    feat: add Python solution and Excalidraw diagram for Group Anagrams

---
+++ 2026/sep-16/49. Group Anagrams.py
@@ -0,0 +1,23 @@
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+
+        mapping = {}
+        for str in strs:
+            sorted_str = ''.join(sorted(str))
+
+            if sorted_str in mapping:
+                mapping[sorted_str].append(str)
+            else:
+                mapping[sorted_str] = [str]
+
+        return list(mapping.values())
+
+
+s = Solution()
+
+print("Example 1:", s.groupAnagrams(
+    strs=["eat", "tea", "tan", "ate", "nat", "bat"]))
+# [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+print("Example 2:", s.groupAnagrams(strs=[""]))
+# [[""]]
